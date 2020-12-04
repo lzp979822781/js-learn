fcf40998a1e2eb28e46792e0742af6c31094a219
liuzhipeng26 <liuzhipeng26@jd.com>
2020-12-04 10:09:55 +0800

    add LazyMan

---
+++ js/windowerror.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    window.addEventListener("error", function() {
+        console.log("arguments", arguments);
+        return true;
+    })
+}
+
+for(let i = 0; i < L; i++) {
+
+}
