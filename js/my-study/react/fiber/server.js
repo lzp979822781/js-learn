14ffab99364b995b5109fdf05209f52e6faedb3b
liuzhipeng03 <liuzhipeng03@baidu.com>
2021-12-08 19:42:07 +0800

    add fiber test project

---
+++ js/my-study/react/fiber/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const template = `
+    <html>
+        <head>
+            <title>React Fiber</title>
+        </head>
+        <body>
+        <div id="root"></div>
+        </body>
+    </html> 
+`;
+
+app.get('*', (req, res) => {
+    res.send(template);
+});
+
+app.listen(3000, () => console.log('server is running'));
---
+++ js/my-study/react/fiber/webpack.config.server.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    target: 'node',
+    mode: 'development',
+    entry: './server.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'server.js',
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    externals: [nodeExternals()]
+};
